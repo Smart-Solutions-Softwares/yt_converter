fcfdf6c9ee69cca988e25dd1e584bcaaffd99846
Abdelrahman Ashraf <abdelrahmanashraf25102000@gmail.comm>
2022-02-28 18:20:51 +0200

    Convert YouTube to MP3 file

---
+++ untitled6.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+
+from pytube import YouTube
+import os
+
+import requests
+
+""" The first API """
+url = "https://youtube-to-mp32.p.rapidapi.com/api/yt_to_mp3"
+
+querystring = {"video_id":"edPREMPZ5RA"}
+
+headers = {
+    'x-rapidapi-host': "youtube-to-mp32.p.rapidapi.com",
+    'x-rapidapi-key': "7fd28c8244msh192131f69ff095ep140a0ajsn0a5f135310eb"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+data = response.json()
+
+""" The second API """
+
+url2 = "https://youtube-to-mp34.p.rapidapi.com/metadata/P0PBuWFeTY4"
+
+headers2 = {
+    'x-rapidapi-host': "youtube-to-mp34.p.rapidapi.com",
+    'x-rapidapi-key': "7fd28c8244msh192131f69ff095ep140a0ajsn0a5f135310eb"
+    }
+
+response2 = requests.request("GET", url, headers=headers)
+
+data2 = response2.json()
+print(data2)
+
+yt = YouTube(data["Full_Video_Link"])
+
+try:
+  print("Downloading.......")
+  video = yt.streams.filter(only_audio=True).first()
+  out_file = video.download()
+
+  base, ext = os.path.splitext(out_file)
+
+  new_file = base + '.mp3'
+  os.rename(out_file, new_file)
+  print("Successfully downloaded")
+
+except:
+  print(data2["message"])
+
