2076116946f12ea162b67805bf8c5498f396eb58
Abdelrahman Ashraf <abdelrahmanashraf25102000@gmail.comm>
2022-03-02 16:16:40 +0200

    Convert YouTube video to mp3

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, url_for, redirect, send_file, session
+from pytube import YouTube
+from io import BytesIO
+import os
+
+
+import youtube_dl
+
+def run():
+    video_url = input("please enter youtube video url:")
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url = video_url,download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete... {}".format(filename))
+
+if __name__=='__main__':
+    run()
