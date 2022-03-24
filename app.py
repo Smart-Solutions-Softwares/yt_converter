2c4210ff9e04836a3618b1a8c41bf003011cbf5d
Sandy <anachrisandra@gmail.com>
2022-03-25 01:15:38 +0800

    Modified for MP3 Download

--- app.py
+++ app.py
@@ -1,33 +1,35 @@
-from flask import Flask, render_template, request, url_for, redirect, send_file, session
-from pytube import YouTube
-from io import BytesIO
-import os
+from flask import Flask, render_template, request
 import youtube_dl
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def my_form():
     return render_template('my-form.html')
 
-@app.route('/', methods=['POST'])
+
+@app.route('/download', methods=['POST'])
 def my_form_post():
-    text = request.form['text']
-    video_info = youtube_dl.YoutubeDL().extract_info(
-        url = text,download=False
-    )
-    filename = f"{video_info['title']}.mp3"
-    options={
-        'format':'bestaudio/best',
-        'keepvideo':False,
-        'outtmpl':filename,
+    yt_url = request.form['text']
+    y = {'nocheckcertificate': True}
+
+    video_info = youtube_dl.YoutubeDL(y).extract_info(
+        url=yt_url, download=False)
+    mp3_filename = f"{video_info['title']}.mp3"
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': mp3_filename,
+        'nocheckcertificate': True
     }
 
-    with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download([video_info['webpage_url']])
+    youtube_dl.YoutubeDL(options).download([yt_url])
 
-    print("Download complete... {}".format(filename))
+    print("Download complete...")
 
     return "Downloaded Successfully"
 
 
+if __name__ == "__main__":
+    app.run(debug=True)
