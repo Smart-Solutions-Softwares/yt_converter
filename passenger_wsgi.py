dff0f15d961ad9e6f7c4575d1afb4f87f2fb7f64
Sandy <anachrisandra@gmail.com>
2022-06-14 23:34:40 +0800

    server updates

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,10 @@
 import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'ytmp3mp4_app', 'bin', 'python')
+INTERP = os.path.join(os.environ['HOME'], 'ytmp3mp4_app2', 'bin', 'python')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
 
 
-sys.path.append('ytmp3mp4_app')
-from ytmp3mp4_app.app import app as application
+sys.path.append('ytmp3mp4_app2')
+from ytmp3mp4_app2.app import app as application
 
